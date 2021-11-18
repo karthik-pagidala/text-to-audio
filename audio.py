8f7585bad2e9fb118b1594eb5c0474f28ba0f1db
karthik-pagidala <94556361+karthik-pagidala@users.noreply.github.com>
2021-11-18 15:03:07 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -2,7 +2,7 @@ from gtts import gTTS
 from playsound import playsound
 audio="speech.mp3"
 language='en'
-sp=gTTS(text="Advance Happy Birthday Natraj Sir",lang=language,slow=False)
+sp=gTTS(text="Good Morning Friends, Welcome to My Github",lang=language,slow=False)
 sp.save(audio)
 playsound(audio)
 print("=========Audio is Playing")
